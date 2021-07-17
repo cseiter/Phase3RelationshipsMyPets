d59bff69dc34e716eedaeeb30d6c24420f9df1c6
chris <cseiter@gmail.com>
2021-07-17 19:06:39 -0400

    build out initial owner Class, lots of tests passed

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,22 @@
 class Owner
   # code goes here
+
+  attr_reader :name, :species
+
+  @@all = []
+
+  def initialize(name)
+    @name=name
+    @species = "human"
+    save
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
 end
